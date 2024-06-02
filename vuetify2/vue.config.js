0de2d8ede5097a73c666a303bde1aacc04e9bec2
ou14500 <unipost1@nate.com>
2024-06-02 18:13:52 +0900

    뷰티파이추가

---
+++ vuetify2/vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: [
+    'vuetify'
+  ],
+  //S: 추가했음
+  devServer: {
+    client: {
+      overlay: false
+    }
+  }
+  //E: 추가했음
+})
